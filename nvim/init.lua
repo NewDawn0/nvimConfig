021835701cb8da34538992c38243902ce6596fab
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-16 02:26:15 +1000

    Add startup time

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
+-- @NOTE: Must be loaded in this exact order
 require("util")
 require("cfg.b16")
 require("cfg.alpha")
+
+local loader = require("loader")
+loader.setup()
+loader.load({
+	["vim-startuptime"] = {
+		cmds = { "StartupTime" },
+	},
+})
