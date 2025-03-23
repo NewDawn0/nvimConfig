0a3d6abf0a718658d7c1fea5b7a2737d5a3af722
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-23 16:41:51 +1000

    use new nixUtils version

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Fully setup runnable neovim configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    cmp-locked = { url = "github:NewDawn0/nvimCmpLocked"; };
-    b16 = {
+    cmp-locked.url = "github:NewDawn0/nvimCmpLocked";
+    utils.url = "github:NewDawn0/nixUtils";
+    b16-nvim = {
       url = "github:NewDawn0/b16.nvim";
       inputs.utils.follows = "utils";
     };
@@ -11,35 +11,26 @@
       url = "github:NewDawn0/build.nvim";
       inputs.utils.follows = "utils";
     };
-    utils = {
-      url = "github:NewDawn0/nixUtils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    loader = {
+    loader-nvim = {
       url = "github:NewDawn0/loader.nvim";
       inputs.utils.follows = "utils";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, utils, nixpkgs, ... }@inputs:
+  outputs = { self, utils, ... }@inputs:
     let
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.b16.overlays.default
-            inputs.build-nvim.overlays.default
-            inputs.loader.overlays.default
-            inputs.cmp-locked.overlays.default
-            (final: prev: (import ./nix/overlays.nix { inherit prev; }))
-          ];
-        };
+      overlays = with inputs; [
+        b16-nvim.overlays.default
+        build-nvim.overlays.default
+        loader-nvim.overlays.default
+        cmp-locked.overlays.default
+        (final: prev: (import ./nix/overlays.nix { inherit prev; }))
+      ];
     in {
       overlays.default = final: prev: {
         ndnvim = self.packages.${prev.system}.default;
       };
-      packages = utils.lib.eachSystem { inherit mkPkgs; } (pkgs:
+      packages = utils.lib.eachSystem { inherit overlays; } (pkgs:
         let
           opts = import ./nix/opts.nix;
           nvim = import ./nix/nvim.nix { inherit pkgs; };
