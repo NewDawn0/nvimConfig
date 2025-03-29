fa0d84c0e73079e80fef4ad554063daa783b882e
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-29 14:57:45 +1000

    add treesitter textobjects

--- nvim/lua/cfg/ts.lua
+++ nvim/lua/cfg/ts.lua
@@ -1,6 +1,66 @@
+-- Load after plugins
+vim.cmd("packadd nvim-treesitter-textobjects")
 require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
 	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+				["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = { ["<leader>a"] = "@parameter.inner" },
+			swap_previous = { ["<leader>A"] = "@parameter.inner" },
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = { query = "@class.outer", desc = "Next class start" },
+				["]o"] = "@loop.*",
+				["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+				["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+			goto_next = { ["]d"] = "@conditional.outer" },
+			goto_previous = { ["[d"] = "@conditional.outer" },
+		},
+		lsp_interop = {
+			enable = true,
+			border = "none",
+			floating_preview_opts = {},
+			peek_definition_code = {
+				["<leader>df"] = "@function.outer",
+				["<leader>dF"] = "@class.outer",
+			},
+		},
+	},
 })
+
+-- Repeatable move selections
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
